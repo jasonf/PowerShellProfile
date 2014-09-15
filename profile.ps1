32f21824ef2591e1f7930644bee2ed48797918e5
Jason Fiset <jafiset@gmail.com>
2014-09-14 20:55:17 -0700

    Added color options for Administrator Shell

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,32 @@
 # Profile is saved at "<My Documents>\WindowsPowerShell\profile.ps1"
 
-# Load Windows PowerShell cmdlets for managing vSphere
-Add-PSSnapin VMware.VimAutomation.Core
+# Change background color on all elevated command prompts (from ilovepowershell.com):
+# http://www.ilovepowershell.com/profile-trick-how-to-set-administrator-mode-background-color/
+if ($host.UI.RawUI.WindowTitle -match "Administrator")
+{
+    $host.UI.RawUI.BackgroundColor = "DarkRed";
+    $host.UI.RawUI.ForegroundColor = "White"
+} else {
 
-# Move to the Documents folder
-cd Documents
+    # Load Windows PowerShell cmdlets for managing vSphere
+    Add-PSSnapin VMware.VimAutomation.Core
 
-# This returns the prompt once everything is loaded, otherwise, it looks like powershell just hangs.
-echo ""
+    # Move to the Documents folder
+    cd Documents
+
+    # This returns the prompt once everything is loaded, otherwise, it looks like powershell just hangs.
+    echo ""
+
+   #$MaximumHistoryCount = 10KB
+
+   #function bye 
+   #{
+   #    Get-History -Count 10KB |Export-CSV ~\.ps_history
+   #    exit
+   #}
+
+   #if (Test-path ~\.ps_history)
+   #{
+   #    Import-CSV ~\.ps_history |Add-History
+   #}
+}
